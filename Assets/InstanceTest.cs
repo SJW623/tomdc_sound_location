8c3988634c02e077de80a04e32b0094898359a25
Sam Winkelstein <sjwinkels44@gmail.com>
2016-07-22 15:52:23 -0400

    initial commit

---
+++ Assets/InstanceTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstanceTest : MonoBehaviour {
+    public GameObject projectile;
+    public GameObject ball;
+    public float speed;
+    public Transform cam;
+    int timer;
+    // Use this for initialization
+    void Start () {
+        timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.LookAt(cam);
+        if (timer >= 100)
+        {
+            Instantiate(projectile, gameObject.transform.position, gameObject.transform.rotation);
+
+            
+
+            timer = 0;
+        }
+        else
+        {
+            timer++;
+        }
+	}
+}
---
+++ Assets/move.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+    int timer;
+    public Transform cam;
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+
+	}
+    // Update is called once per frame
+    void Update() {
+
+        transform.Translate(transform.TransformDirection(Vector3.forward) * 3 * Time.deltaTime);
+        if(timer >= 1000)
+        {
+            Destroy(gameObject);
+        }
+        timer++;
+    }
+}
